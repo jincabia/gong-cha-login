733007bd5f08ebab726514e9c5c5f8ef6832a138
jincabia <140627796+jincabia@users.noreply.github.com>
2024-04-18 14:22:01 -0600

    fixed firebase, added process.env api keys

--- app/src/_utils/firebase.js
+++ app/src/_utils/firebase.js
@@ -7,14 +7,14 @@ import {getAuth} from 'firebase/auth';
 
 
 // // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-//   };
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
 // // Initialize Firebase
 
 
@@ -23,14 +23,14 @@ import {getAuth} from 'firebase/auth';
 
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC51iD4UHDGJH3Os4IZeQv6GNuvM3EMYFc",
-  authDomain: "gong-cha---demo.firebaseapp.com",
-  projectId: "gong-cha---demo",
-  storageBucket: "gong-cha---demo.appspot.com",
-  messagingSenderId: "250475008218",
-  appId: "1:250475008218:web:324655c80256200d8515e3"
-};
+// const firebaseConfig = {
+//   apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: NEXT_PUBLIC_FIREBASE_APP_ID
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -38,41 +38,9 @@ const app = initializeApp(firebaseConfig);
 export const auth=getAuth();
 export default app;
 
-
-
-// import { useEffect, useState } from 'react';
-// import { initializeApp } from 'firebase/app';
-// import { getAuth, onAuthStateChanged } from 'firebase/auth';
-// // import { getFirestore } from 'firebase/firestore';
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC51iD4UHDGJH3Os4IZeQv6GNuvM3EMYFc",
-//   authDomain: "gong-cha---demo.firebaseapp.com",
-//   projectId: "gong-cha---demo",
-//   storageBucket: "gong-cha---demo.appspot.com",
-//   messagingSenderId: "250475008218",
-//   appId: "1:250475008218:web:324655c80256200d8515e3"
-// };
-
-// const firebaseApp = initializeApp(firebaseConfig);
-// // const firestore = getFirestore(/firebaseApp);
-// const auth = getAuth(firebaseApp);
-
-// function MyApp({ Component, pageProps }) {
-//   const [user, setUser] = useState(null);
-
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (user) => {
-//       setUser(user);
-//     });
-
-//     return () => unsubscribe();
-//   }, []);
-
-//   return (
-//     <Component {...pageProps} firebase={firebaseApp} auth={auth} user={user} />
-//   );
-// }
-
-// export default MyApp;
-// export {firebaseApp}
+// NEXT_PUBLIC_FIREBASE_API_KEY="AIzaSyCg20CvbIEwd6BIHd4GNAUMcPfhNIO0kPk"
+// NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN="gong-cha---demo.firebaseapp.com"
+// NEXT_PUBLIC_FIREBASE_PROJECT_ID="gong-cha---demo"
+// NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET="gong-cha---demo.appspot.com"
+// NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID="250475008218"
+// NEXT_PUBLIC_FIREBASE_APP_ID="1:250475008218:web:324655c80256200d8515e3"
